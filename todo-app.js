4fb482c2d28318d385e9aabbe547e758b9a750a2
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-12-16 18:26:57 +0530

    sample jsx element for TinyReact

--- todo-app.js
+++ todo-app.js
@@ -3,3 +3,22 @@
 /*** step 1,2,3,4 - createElement */
 
 const root = document.getElementById('root');
+
+var step1 = (
+	<div>
+		<h1 className='header'>Hello Tiny React!</h1>
+		<h2>(coding nirvana)</h2>
+		<div>
+			nested 1<div>nested</div>1.1
+		</div>
+		<h3>(OBSERVE: This will change)</h3>
+		{2 == 1 && <div>Render this if 2==1</div>}
+		{2 == 2 && <div>{2}</div>}
+		<span>This is a text</span>
+		<button onClick={() => alert('Hi')}>Click Me!</button>
+		<h3>This will be deleted</h3>
+		2,3
+	</div>
+);
+
+console.log(step1);
