47f5b6ea0ab0e0eeecfd3bbd21d982f0cffb2bca
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-12-16 18:00:43 +0530

    Initial setup

---
+++ steps.js
@@ -0,0 +1,21 @@
+// 1. createElement Stub
+// 2. createElement Basic Implementation
+// 3. createElement Handle true/false short circuiting
+// 4. createElement Remove undefined nodes (because of true/false short circuits)
+// 5. render native dom elements (only top level)
+// 6. rendering child elements
+// 7. setting DOM attributes and events
+// 8. diffing native dom elements
+// 9. deleting extra dom nodes
+// 10. rendering functional components
+// 11. passing props to functional component
+// 12. nest functional components (no change in code here -> check demo app)
+// 13. diffing functional components (just remove extra nodes for now)
+// 14. render stateful component
+// 15. implement setState method in base component
+// 16. setState functional param
+// 17. diffing stateful components
+// 18. Adding ref support (TODO)
+// 19. Optimize diff: Old is component and new is native
+// 20. Add support for react event onDoubleClick (we support native onDblClick)
+// 21. Adding support for keys
